de94ef3dc11a79072867a3aef01bb6a23b6df6e1
Maurits Erkendiep <maurits@superscanner.nl>
2025-03-26 20:14:39 +0100

    Move images to public folder + Add heroicons package + Add some icons to homepage

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { AcademicCapIcon, HeartIcon, HomeIcon, UserGroupIcon } from '@heroicons/react/24/solid/index.js';
 
 const HeroSection = () => {
     return (
-        <section className="bg-[url(/img.png)] pt-32 pb-16 w-full">
+        <section className="bg-[url(/images/hero-home.png)] pt-32 pb-16 w-full">
             <div className="container mx-auto px-6 relative z-20 text-white">
                 <div className="max-w-2xl">
                     <h1 className="text-4xl font-bold mb-4">Door studenten. Voor studenten.</h1>
@@ -11,27 +12,19 @@ const HeroSection = () => {
 
                     <div className="space-y-2">
                         <div className="flex items-center">
-                            <div
-                                className="w-6 h-6 flex items-center justify-center bg-green-500 rounded-full mr-2">✓
-                            </div>
+                            <UserGroupIcon class={'size-6 mr-2'}/>
                             <span>1-op-1 support</span>
                         </div>
                         <div className="flex items-center">
-                            <div
-                                className="w-6 h-6 flex items-center justify-center bg-green-500 rounded-full mr-2">✓
-                            </div>
+                            <AcademicCapIcon class={'size-6 mr-2'}/>
                             <span>Studieruimtes</span>
                         </div>
                         <div className="flex items-center">
-                            <div
-                                className="w-6 h-6 flex items-center justify-center bg-green-500 rounded-full mr-2">✓
-                            </div>
+                            <HomeIcon class={'size-6 mr-2'}/>
                             <span>Zaalverhuur</span>
                         </div>
                         <div className="flex items-center">
-                            <div
-                                className="w-6 h-6 flex items-center justify-center bg-green-500 rounded-full mr-2">✓
-                            </div>
+                            <HeartIcon class={'size-6 mr-2'}/>
                             <span>Mental health events</span>
                         </div>
                     </div>
@@ -39,12 +32,12 @@ const HeroSection = () => {
                     <div className="mt-8 flex space-x-4">
                         <button className="btn btn-primary">
                             Lees verder
-                            <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                      d="M9 5l7 7-7 7"/>
+
+                            <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7"/>
                             </svg>
                         </button>
+
                         <button className="btn btn-secondary">
                             Neem contact op!
                         </button>
