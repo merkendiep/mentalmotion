10568b462b2bd6c8082e69cd5d99b63b5c1d55bb
Maurits Erkendiep <maurits@superscanner.nl>
2025-04-02 18:12:53 +0200

    Try to fix vercel part 2

--- vite.config.js
+++ vite.config.js
@@ -15,8 +15,4 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
-    // Add this to ensure Vercel understands your project structure
-    build: {
-        outDir: 'dist',
-    }
 })
