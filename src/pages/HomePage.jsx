336cc66359572ecb107b1da951a1fb3482aa7cd7
Maurits Erkendiep <maurits@superscanner.nl>
2025-04-02 22:04:48 +0200

    Try to fix vercel part 17

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Navbar from '@src/components/Navbar.jsx'
-import Hero from '@src/components/Hero.jsx';
-import Services from '@src/components/Services.jsx';
-import Footer from '@src/components/Footer.jsx';
-import TransitionWithBorder from '@src/components/TransitionWithBorder.jsx';
+import Navbar from '/src/components/Navbar.jsx'
+import Hero from '/src/components/Hero.jsx';
+import Services from '/src/components/Services.jsx';
+import Footer from '/src/components/Footer.jsx';
+import TransitionWithBorder from '/src/components/TransitionWithBorder.jsx';
 
 const HomePage = () => {
     return (
